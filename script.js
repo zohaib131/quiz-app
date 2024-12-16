c1f04d79e635c4d231d84f69f373261853415bdc
zohaib131 <zn058098@gmail.com>
2024-12-16 15:44:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+const quizData=[
+    {
+        question: "HTML stands for ?",
+        a:"hypertext Markdown Language",
+        b:"Hypertext Machine Language",
+        c:"Helicopter Terminal Machine",
+        d:"Hypertext Markup Language",
+        correct:"d",
+
+    },
+    {
+        question:"Quaid-e-Azam was born in _ ?",
+        a:"Faisalabad",
+        b:"Karachi",
+        c:"Lahore",
+        d:"Gujranwala",
+        correct:"b",
+
+    },
+    {
+       question:"Who is our National poet ?",
+       a:"Illama Muhammad Iqbal",
+       b:"Mirza Ghalib",
+       c:"Sarsayed Ahmed Khan",
+       d:"Meer Tki Meer",
+       correct:"a"
+    },
+    {
+        question:"What is our National language?",
+        a:"Pashto",
+        b:"Punjabi",
+        c:"Urdu",
+        d:"Farsi",
+        correct:"c",
+    },
+   
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls=document.querySelectorAll('.answer')
+const questionEl=document.getElementById('question')
+const a_text=document.getElementById('a_text')
+const b_text=document.getElementById('b_text')
+const c_text=document.getElementById('c_text')
+const d_text=document.getElementById('d_text')
+const submitBtn=document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz(){
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+   
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+
+}
+
+function getSelected(){
+    let answer
+
+    answerEls.forEach(answerEl => {
+  if(answerEl.checked) {
+    answer = answerEl.id
+  }
+    })
+
+    return answer
+}
+
+ submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+
+    if(answer){
+    if(answer === quizData[currentQuiz].correct) {
+        score++
+
+    }
+
+    currentQuiz++
+    if(currentQuiz < quizData.length) {
+        loadQuiz()
+    } else {
+        quiz.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+            <button onclick="location.reload()">Reload</button>
+        `
+    }
+
+    }
+})
